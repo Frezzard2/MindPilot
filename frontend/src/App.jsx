9f36a6fb640f682a34bfe60b4c2afef88c2e7977
Zsombor Kukucska <kukucska.zsombor@gmail.com>
2025-06-19 12:55:46 +0200

    connecting backend with frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { use, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [topic, setTopic] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+  setLoading(true);
+  setResult("");
+
+  try {
+    const response = await axios.post("http://127.0.0.1:8000/api/explain", {
+      topic: topic,
+    });
+    console.log("RESPONSE:", response.data);
+    setResult(response.data.data);
+  } catch (error) {
+    console.error("Error:", error);
+    setResult("Something went wrong.");
+  }
+
+  setLoading(false);
+};
+  // Handle the form submission
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ maxWidth: "600px", margin: "auto", padding: "2rem" }}>
+      <h1>Mindpilot</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          value={topic}
+          onChange={(e) => setTopic(e.target.value)}
+          placeholder="Enter a topic"
+        />
+          <button type="submit">Explain</button>
+      </form>
+
+
+      {result && (
+        <div style={{ marginTop: "2rem", background: "#f5f5f5", padding: "1rem" }}>
+          <h2>Explanation</h2>
+          <p>{result}</p>
+          </div>
+        )}  
+    </div>
+  );
 }
 
-export default App
+export default App;
